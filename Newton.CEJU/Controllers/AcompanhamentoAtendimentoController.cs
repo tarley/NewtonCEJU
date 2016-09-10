4bd77d5ab2c477e3f5899e96f5e92fe0db98b80a
Dorcas Miguel <dorcascv@gmail.com>
2016-09-10 19:08:37 -0300

    Criação tela acompanhamento

---
+++ Newton.CEJU/Controllers/AcompanhamentoAtendimentoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Newton.CJU.Controllers
+{
+    public class AcompanhamentoAtendimentoController : Controller
+    {
+        // GET: AcompanhamentoAtendimento
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
