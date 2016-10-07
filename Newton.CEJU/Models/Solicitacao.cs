ad9b2a22a7e3735f1eb7548fb43b22d143d5fff9
Diogo <Diogo@MATRIX-PC>
2016-10-07 20:53:33 -0300

    Alteracao e validacao da Solicitacao

--- Newton.CEJU/Models/Solicitacao.cs
+++ Newton.CEJU/Models/Solicitacao.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -21,12 +22,26 @@ namespace Newton.CJU.Models
 
         [Required]
         public DateTime DataCadastro { get; set; }
-        [Required]
+
+        [DisplayName("Informe sua dúvida")]
+        [StringLength(100)]
+        [Required(ErrorMessage = "Este campo é obrigatório! Por favor, descreva de forma resumida sua dúvida.")]
         public string Duvida { get; set; }
         public string Parecer { get; set; }
+
+        [DisplayName("Escolha o tipo de assunto")]
+        [Required(ErrorMessage = "Seleção do assunto é obrigatória!")]
         public string FatoJuridico { get; set; }
         public string Fundamentacao { get; set; }
+
+        [DisplayName("Identificação das partes")]
+        [StringLength(100)]
+        [Required(ErrorMessage = "Este campo é obrigatório! Por favor, digite os nomes das partes.")]
         public string IdentificacaoPartes { get; set; }
+
+        [DisplayName("Descrição do caso (500 caracteres)")]
+        [StringLength(500)]
+        [Required(ErrorMessage = "Descrição é obrigatória!")]
         public string Descricao { get; set; }
         public string Correcao { get; set; }
 
